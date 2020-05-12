9c54e7bf9a7f1505e52d01e79c5af342bd948df0
syafzid <18052270@myrp.edu.sg>
2020-05-12 11:11:17 +0800

    Completed with Section B

--- app/src/main/java/com/example/demoexplicitintent/HeroStatsActivity.java
+++ app/src/main/java/com/example/demoexplicitintent/HeroStatsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,9 +26,40 @@ public class HeroStatsActivity extends AppCompatActivity {
         tvStrength = findViewById(R.id.textViewStrength);
         tvTechnicalProwess = findViewById(R.id.textViewTechnical);
 
+        btnLike = findViewById(R.id.buttonLike);
+        btnDislike = findViewById(R.id.buttonDislike);
+
         // Use getters of Hero object to get the attributes
         tvName.setText(hero.getName());
         tvStrength.setText("Strength: " + hero.getStrength());
         tvTechnicalProwess.setText("Technical: " + hero.getTechnicalProwess());
+
+        // When button Like is clicked, set the results accordingly and finish() to close this act
+        btnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create intent & pass data in String data
+                Intent i = new Intent();
+                i.putExtra("like", "like");
+
+                // Set result to RESULT_OK to indicate normal response and pass in the intent containing the like
+                setResult(RESULT_OK, i);
+                finish();
+            }
+        });
+
+        // When button Dislike is clicked, set the results accordingly and finish() to close this act
+        btnDislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create intent & pass data in String data
+                Intent i = new Intent();
+                i.putExtra("like", "dislike");
+
+                // Set result to RESULT_OK to indicate normal response and pass in the intent containing the like
+                setResult(RESULT_OK, i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/demoexplicitintent/MainActivity.java
+++ app/src/main/java/com/example/demoexplicitintent/MainActivity.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvSuperman, tvBatman;
+
+    // These request identify who started the second activity
+    int requestCodeForSupermanStats = 1;
+    int requestCodeForBatmanStats = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +32,47 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, HeroStatsActivity.class);
                 // Put hero object in intent
                 i.putExtra("hero", superman);
-                startActivity(i);
+                startActivityForResult(i, requestCodeForSupermanStats);
             }
         });
+
+        // Set listener to handle the clicking of Superman Text View
+        tvBatman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create Hero object of strength 100 & technical 60
+                Hero batman = new Hero("Batman", 60, 90);
+                Intent i = new Intent(MainActivity.this, HeroStatsActivity.class);
+                // Put hero object in intent
+                i.putExtra("hero", batman);
+                startActivityForResult(i, requestCodeForBatmanStats);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Only handle when 2nd activity closed manually and data contains something
+        if(resultCode == RESULT_OK) {
+            if(data != null) {
+                // Get data passed back from 2nd activity
+                String like = data.getStringExtra("like");
+                String statement = "";
+
+                // If it is activity started by clicking Superman, create corresponding String
+                if(requestCode == requestCodeForSupermanStats) {
+                    statement = "You " + like + " Superman";
+                }
+
+                // If it is activity started by clicking Batman, create corresponding String
+                if(requestCode == requestCodeForBatmanStats) {
+                    statement = "You " + like + " Batman";
+                }
+
+                Toast.makeText(MainActivity.this, statement, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
